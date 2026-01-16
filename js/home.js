217e52029ef1a58ac7f26e93297cd69831dff01c
MTito2 <mateuscunha166@gmail.com>
2026-01-16 13:50:01 -0300

    Inclui lógica de troca de projetos

--- js/home.js
+++ js/home.js
@@ -1,25 +1,12 @@
-// Variáveis
 const projectsItem = document.querySelectorAll(".projects__item")
-export const pageList = new Array
+const pageList = new Array
 const actuallyPage = window.location.pathname.replace("/", "")
-
-console.log(actuallyPage.replace("/", ""))
 
 projectsItem.forEach(element => {
     pageList.push(element.dataset.title + ".html")
 });
 
-
-// Funções
-function foo() {
-    console.log(pageList.indexOf(actuallyPage))
-    console.log(actuallyPage)
-}
-
-foo()
-
-
-// Listeners
+localStorage.setItem("page_projects", JSON.stringify(pageList))
 
 projectsItem.forEach(element => {
     element.addEventListener("click", () => {
--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,42 @@
-import { pageList } from "./home";
+const prevBtn = document.getElementById("prev-project")
+const nextBtn = document.getElementById("next-project")
 
-console.log(pageList)
+const projectPages = JSON.parse(localStorage.getItem("page_projects"))
+const projectNumbers = projectPages.length
+const currentPath = window.location.pathname
+const currentPage = currentPath.split("/").at(-1)
+const currentPageIndex = projectPages.indexOf(currentPage)
+
+prevBtn.addEventListener("click", () => {
+    acessPreviusPage()
+})
+
+nextBtn.addEventListener("click", () => {
+    acessNextPage()
+})
+
+function acessPreviusPage() {
+    let previusIndex = currentPageIndex - 1
+
+    if (previusIndex < 0) {
+        previusIndex = projectNumbers - 1 
+    }
+    
+    const previusProjectPage = projectPages[previusIndex]
+    const path = "/projects/" + previusProjectPage
+
+    window.location.href = path;
+}
+
+function acessNextPage() {
+    let nextIndex = currentPageIndex + 1
+
+    if (nextIndex > (projectNumbers - 1)) {
+        nextIndex = 0 
+    }
+    
+    const nextProjectPage = projectPages[nextIndex]
+    const path = "/projects/" + nextProjectPage
+
+    window.location.href = path;
+}

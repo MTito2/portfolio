c36fef820b951afb25600117fad8a6ac6eb4e3b3
MTito2 <mateuscunha166@gmail.com>
2026-02-07 09:40:07 -0300

    Ajuste de entrada em projetos

--- js/home.js
+++ js/home.js
@@ -39,33 +39,32 @@ projectsItem.forEach(element => {
 localStorage.setItem("page_projects", JSON.stringify(pageList))
 
 
-
 projectsItem.forEach(element => {
     element.addEventListener("click", () => {
         const projectTitle = element.dataset.title
         
         if (projectTitle === "bot-grm") {
-            window.location.href = "/projects/bot-grm.html";
+            window.location.href = "./projects/bot-grm.html";
         }
 
         else if (projectTitle === "bot-pessoal") {
-            window.location.href = "/projects/bot-pessoal.html";
+            window.location.href = "./projects/bot-pessoal.html";
         }
 
         else if (projectTitle === "easyfocus") {
-            window.location.href = "/projects/easyfocus.html";
+            window.location.href = "./projects/easyfocus.html";
         }
 
         else if (projectTitle === "focus-timer") {
-            window.location.href = "/projects/focus-timer.html";
+            window.location.href = "./projects/focus-timer.html";
         }
 
         else if (projectTitle === "flash-gen") {
-            window.location.href = "/projects/flash-gen.html";
+            window.location.href = "./projects/flash-gen.html";
         }
 
         else if (projectTitle === "assistente-do-tempo") {
-            window.location.href = "/projects/assistente-do-tempo.html";
+            window.location.href = "./projects/assistente-do-tempo.html";
         }
 
     })
